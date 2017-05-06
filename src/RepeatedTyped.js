f0d01ae70298865b5d4b9c30639717a00ce83236
Fondson Tran <fondsont@gmail.com>
2017-05-06 18:59:40 -0400

    add new typed files

---
+++ src/RepeatedTyped.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RepeatedTyped extends Component{
+    render(){
+        return(
+            <span className="repeatedTyped"></span>
+        )
+    }
+};
+
+export default RepeatedTyped;
